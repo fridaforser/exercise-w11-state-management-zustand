275986f2bf8a6369417875038279ba48d0dc4b09
fridaforser <fridaforser@gmail.com>
2024-04-16 14:45:17 +0200

    Rendered TodoList

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { TodoList } from "./components/TodoList.jsx"
+
 export const App = () => {
   return (
     <div className="App">
-      <h1>Render your TodoList here 🤓</h1>
+      <TodoList />
     </div>
   )
 }
